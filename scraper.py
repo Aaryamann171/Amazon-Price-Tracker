3024258c5449632c7c05e02c5b9e82402a83d389
Aaryamann Sharma <aaryamann171@gmail.com>
2019-09-05 14:34:28 +0530

    second commit

--- scraper.py
+++ scraper.py
@@ -1,1 +1,13 @@
-print("Hello, scraper here. who this?")
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.amazon.in/BenQ-23-8-inch-Monitor-Built/dp/B073NTCT4Q/ref=sr_1_3?keywords=24+inch+monitor&qid=1567673325&s=gateway&sr=8-3'
+
+headers = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+
+page = requests.get(URL, headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+title = soup.find(id="productTitle")
+
+print(title)
